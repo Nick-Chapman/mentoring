8d19ff3c21713a9343a4108efea8ffda28c04ace
Nick Chapman <nic@amadido.co.uk>
2022-10-31 12:06:04 +0000

    kill play-lex-yacc

--- play-lex-yacc/lexer.h
+++
@@ -1,2 +0,0 @@
-
-void lex_entry(char*);
--- play-lex-yacc/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "lexer.h"
-
-int main(int argc, char* argv[]) {
-  printf("play lex and yacc..\n");
-  lex_entry("the quick 42 fox\nblah");
-  return 0;
-}
