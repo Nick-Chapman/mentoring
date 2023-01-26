8899de6d29e175b862a2c0c3bfc67c2d6ffe311b
Nick Chapman <nic@amadido.co.uk>
2023-01-26 17:41:13 +0000

    interpreter: sub/mul; let-expressions

--- h101/src/Interpreter.hs
+++ h101/src/Interpreter.hs
@@ -1,10 +1,9 @@
 module Interpreter (main) where
 
-import Prelude hiding (lookup)
-import Text.Printf
-
-import qualified Data.Map as Map (fromList,lookup)
 import Data.Map (Map)
+import Prelude hiding (lookup)
+import Text.Printf (printf)
+import qualified Data.Map as Map (fromList,lookup,insert)
 
 main :: IO ()
 main = do
@@ -15,7 +14,7 @@ main = do
       putStr (show sam ++ " : ")
       let actual = eval env0 sam
       let ok = (expected==actual)
-      let msg = if ok then "PASS" else printf "FAIL: expected=%d, but got=%d" expected actual
+      let msg = if ok then printf "PASS: %d" actual else printf "FAIL: expected=%d, but got=%d" expected actual
       putStrLn msg
 
     sams :: [(Exp,Int)]
@@ -23,15 +22,22 @@ main = do
       [ (Add (Lit 1) (Add (Lit 2) (Lit 3))
         , 6
         )
-      , (Add (Lit 1) (Add (Var "x") (Lit 3))
-        , 104
-        )
+      , (Let "x" (Sub (Lit 10) (Lit 3))
+         (Mul (Var "x") (Var "x"))
+        , 49)
+      , (Let "x" (Lit 5) (Mul (Let "x" (Add (Var "x") (Var "x")) (Sub (Var "x") (Lit 1))) (Var "x"))
+        , 45)
+      , (Let "x" (Lit 5) (Let "y" (Add (Var "x") (Lit 1)) (Mul (Var "x") (Var "y")))
+        , 30)
       ]
 
 data Env = Env (Map Identifier Value)
 
 env0 :: Env
-env0 = Env (Map.fromList [("x",100)])
+env0 = Env (Map.fromList [])
+
+extend :: Env -> Identifier -> Value -> Env
+extend (Env m) x v = Env (Map.insert x v m)
 
 lookup :: Env -> Identifier -> Value
 lookup (Env m) x =
@@ -47,15 +53,29 @@ eval env = \case
   Lit n -> n
   Var x -> lookup env x
   Add l r -> eval env l + eval env r
+  Sub l r -> eval env l - eval env r
+  Mul l r -> eval env l * eval env r
+  Let x rhs body -> do
+    let v = eval env rhs
+    let env' = extend env x v
+    eval env' body
 
 data Exp
   = Lit Int
   | Var Identifier
   | Add Exp Exp
+  | Sub Exp Exp
+  | Mul Exp Exp
+  | Let Identifier Exp Exp
 
 instance Show Exp where
   show :: Exp -> String
   show = \case
     Lit n -> show n
     Var x -> x
-    Add l r -> "(" ++ show l ++ "+" ++ show r ++ ")"
+    Add l r -> bin "+" l r
+    Sub l r -> bin "-" l r
+    Mul l r -> bin "*" l r
+    Let x rhs body -> "(let " ++ x ++ " = " ++ show rhs ++ " in " ++ show body ++ ")"
+
+    where bin op l r = "(" ++ show l ++ op ++ show r ++ ")"
