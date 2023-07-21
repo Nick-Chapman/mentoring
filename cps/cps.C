2c27c8e6de9b02f7e60c2620fa4390e958d45ffe
Nick Chapman <nic@amadido.co.uk>
2023-07-21 13:46:27 +0100

    cps.C (done)

--- cps/cps.C
+++ cps/cps.C
@@ -9,12 +9,60 @@ int fact (int n) {
   }
 }
 
-int fib (int n) {
+/*int fib (int n) {
   if (n<2) {
     return n;
   } else {
     return fib(n-1) + fib(n-2);
   }
+}*/
+
+class FK {
+public:
+  virtual int apply(int) =0;
+};
+
+int fibD (int n, FK* k);
+
+class KDone : public FK {
+public:
+  int apply(int res) {
+    return res;
+  }
+};
+
+struct KTwo : public FK {
+  FK* k;
+  int res1;
+  int apply(int res2) {
+    return k->apply(res1+res2);
+  }
+};
+
+struct KOne : public FK {
+  int n;
+  FK* k;
+  int apply(int res1) {
+    KTwo* k2 = new KTwo;
+    k2->res1 = res1;
+    k2->k = k;
+    return fibD(n-2, k2);
+  }
+};
+
+int fibD (int n, FK* k) {
+  if (n < 2) {
+    return k->apply(n);
+  } else {
+    KOne* k1 = new KOne;
+    k1->n = n;
+    k1->k = k;
+    return fibD(n-1, k1);
+  }
+}
+
+int fib (int n) {
+  return fibD(n, new KDone);
 }
 
 int main() {
