5431b1920a936181003527ba1c17c9201f2072c5
Nick Chapman <nic@amadido.co.uk>
2025-07-31 12:13:53 +0100

    tidy top

--- h101/src/Top.hs
+++ h101/src/Top.hs
@@ -23,9 +23,7 @@ import qualified Fork
 
 main :: IO ()
 main = do
-  --putStrLn "*h101*"
   let _ = Interpreter.main
-  --print prog
   let _ = Bytecode.main
   let _ = MotivateStateMonad.main
   let _ = OldBf.main
@@ -43,36 +41,6 @@ main = do
   let _ = Master3.main
   let _ = RegAlloc.main
   let _ = Lisp.main
-
   let _ = PrepGreen.main
   Fork.main
   pure ()
-
-
-_prog :: Int
-_prog = do
-  let fact = fix (\fact -> \n -> if n < 1 then 1 else n * fact (n-1))
-  1 + fact (2+3)
-
-  where
-    fix :: ((a->a) -> (a->a)) -> (a->a)
-    --fix :: (g -> g) -> g
-    fix openDefinition = do
-      let func x = openDefinition func x
-      func
-
-{-
-prog :: Int
-prog = do
-  --let square x = x * x
-  let inc x = x + 1
-
-  --let compose f g x = f (g x)
-  --let twice = \f -> \x -> f (f x)
-  let thrice = \f -> \x -> f (f (f x))
-
-  --let r = thrice (thrice inc) 0
-  let r = thrice thrice inc 0
-
-  r
--}
